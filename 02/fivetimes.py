5bd16dff466e26c038a4d5382a89edba8fe09864
Frank Larios <frankandrewlarios@gmail.com>
2017-11-29 23:20:37 -0800

    adds scripts from section 02

---
+++ 02/fivetimes.py
@@ -0,0 +1,9 @@
+# The range function can take up to 3 arguments separated by commas:
+# a starting value, a value that it goes up to but doesn't include,
+# and a "step" value, that is, an increment the loop is increased 
+# by each time it makes a pass. The first value by itself it the total number of times it will loop.
+print('My name is')
+#for i in range(5, 10)
+#for i in range(0, 10, 2)
+for i in range(5): 
+    print('Jimmy Five Times ' + str(i))
